19db34e43c369579815037463b5b74dadf67a8c1
Sylvain Gemieux <44305327+syl20pro@users.noreply.github.com>
2022-01-17 16:02:23 +0100

    Add[First version]:variables & function - Ajout des parties de présentation et origine du projet - Déclaration des variables avec vérification d erreur de saisie

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # py-access
+
+## Présentation du projet
+
+
+## Origine du projet
+Mise en pratique du tuto Python sur Youtube : https://youtu.be/oUJolR5bX6g?list=PLCJ49tqkqxxAWGXWjBw9Fx-to6YJxTe5i
+par Jonathan de la chaîne codeavecjonathan.
+
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Python est installé. tout fonctionne bien")
+""" Mise en pratique du tuto Python sur Youtube : https://youtu.be/oUJolR5bX6g?list=PLCJ49tqkqxxAWGXWjBw9Fx-to6YJxTe5i
+par Jonathan de la chaîne codeavecjonathan """
+
+print("Bienvenue dans le programme Py Access")
+
+# Déclaration des variables
+prenom1 = input("Quel est votre prénom ? ")
+age1 = input("Bonjour, " + prenom1 + ", quel âge avez-vous ? ")
+try:
+    age_n = int(age1) + 1
+except:
+    print("ERREUR : Vous devez rentrer votre âge au format nombre.")
+else:
+    print("Votre prénom est " + prenom1 + ", vous avez " + str(age1) + " ans.")
+    print("L'an prochain, vous aurez " + str(age_n) + " ans.")
