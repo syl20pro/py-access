4100a92b814e4b50f00b837818a994e7aca8e0a2
Sylvain Gemieux <44305327+syl20pro@users.noreply.github.com>
2022-01-17 16:53:33 +0100

    Add[function]:while
    -Ajout d'une boucle while

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ par Jonathan de la chaîne codeavecjonathan """
 print("Bienvenue dans le programme Py Access")
 
 # Déclaration des variables
+
 prenom1 = input("Quel est votre prénom ? ")
 age1 = input("Bonjour, " + prenom1 + ", quel âge avez-vous ? ")
 try:
@@ -11,5 +12,10 @@ try:
 except:
     print("ERREUR : Vous devez rentrer votre âge au format nombre.")
 else:
+    charge = 0
+    while charge <= 100:
+        print("Chargement en cours " + str(charge) + "%")
+        charge = charge + 13
+    print("Chargement terminé")
     print("Votre prénom est " + prenom1 + ", vous avez " + str(age1) + " ans.")
     print("L'an prochain, vous aurez " + str(age_n) + " ans.")
